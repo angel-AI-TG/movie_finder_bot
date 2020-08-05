c8011c4cd61131c6cdf97afc3433be2e720f1ade
xditya <58950863+xditya@users.noreply.github.com>
2020-08-05 12:44:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 '''Token of bot, TMDB API key'''
 
+import os
 
 # Token of Telegram bot
-TOKEN = ""
-
+TOKEN=os.environ.get("TOKEN","129493984anhsbdvjsfabjs")
 
 # API key of TMDB (v3 auth)
-API_KEY= ""
+API_KEY=os.environ.get("API_KEY",None)
