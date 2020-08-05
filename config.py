b712fcd4139e967ba748d232ec04fa6c5c6a715b
xditya <58950863+xditya@users.noreply.github.com>
2020-08-05 12:53:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 '''Token of bot, TMDB API key'''
 
+from moviebot import TOKEN, API_KEY
 import os
 
+TKN = str(TOKEN) if TOKEN else "129493984anhsbdvjsfabjs"
+API = str(API_KEY) if API_KEY else ""
+
 # Token of Telegram bot
-TOKEN=os.environ.get("TOKEN","129493984anhsbdvjsfabjs")
+TOKEN= TKN
 
 # API key of TMDB (v3 auth)
-API_KEY=os.environ.get("API_KEY",None)
+API_KEY=os.environ.get(API)
